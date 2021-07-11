67e964f369045a55f167e40717d06e198b9a64df
ElenaTsap <elenatsapaki@gmail.com>
2021-07-11 21:02:03 +0200

    changed styling

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -3,10 +3,11 @@ import BGImage from './images/natalia-y.jpg'
 
 export const GlobalStyle = createGlobalStyle `
     html {
-        height: 100%;
+        height: 100vh;
     }
 
     body {
+        height: 100vh;
         background-image: url(${BGImage});
         background-size: cover;
         margin: 0;
@@ -31,21 +32,14 @@ export const Wrapper = styled.div`
     }
 
     .score {
-        color: #fff;
+        color: #000;
         font-size: 2rem;
         margin: 0;
     }
 
     h1 {
-        font-family: sans-serif;
-        background-image: linear-gradient(180deg, #fff, #87f1ff);
+        font-family: 'Otomanopee One', sans-serif;
         background-size: 100%;
-        background-clip: text;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        -moz-background-clip: text;
-        -moz-text-fill-color: transparent;
-        filter: drop-shadow(2px 2px #0085a3);
         font-size: 70px;
         text-align: center;
         margin: 20px;
@@ -53,13 +47,13 @@ export const Wrapper = styled.div`
 
     .start, .next {
         cursor: pointer;
-        background: linear-gradient(180deg, #fff, #ffcc91);
-        border: 2px solid #d38558;
         box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
-        border-radius: 10px;
         height: 40px;
         margin: 20px 0;
         padding: 0 40px;
+        background: black;
+        color: white;
+        border: none;
     }
 
     .start {
--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     max-width: 1100px;
     background: #ebfeff;
-    border-radius: 10px;
-    border: 2px solid #0085a3;
+    border: 4px solid black;
     padding: 20px;
     box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
     text-align: center;
@@ -34,15 +33,18 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps> `
         margin: 5px 0;
         background: ${({ correct, userClicked }) => 
             correct 
-            ? 'linear-gradient(90deg, #56ffa4, #59bc86)' 
+            ? 'green' 
             : !correct && userClicked 
-            ? 'linear-gradient(90deg, #ff5656, #c16868)'
-            : 'linear-gradient(90deg, #56ccff, #6eafb4)'
+            ? 'red'
+            : 'white'
         };
-        border: 3px solid #fff;
-        box-shadow: 1px 2px 0px rgba(0, 0, 0, 0.1);
-        border-radius: 10px;
-        color: #fff;
+        border: 2px solid #000;
+        box-shadow: 1px 2px 0px black;
+        color: black;
         text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.25);
+
+        :hover {
+            background: orange;
+        }
     }
 `
